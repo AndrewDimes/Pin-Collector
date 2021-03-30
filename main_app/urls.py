691836a9105a4d3164f69a5a0864b012cf127a0d
Andrew Dimes <andrewdimes@gmail.com>
2021-03-30 14:36:42 -0400

    add update and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('pins/', views.pins_index, name='index'),
-    path('pins/<int:pin_id>/', views.pins_detail, name='detail')
+    path('pins/<int:pin_id>/', views.pins_detail, name='detail'),
+    path('pins/create/', views.PinCreate.as_view(), name='pins_create'),
+    path('pins/<int:pk>/update/', views.PinUpdate.as_view(), name='pins_update'),
+    path('pins/<int:pk>/delete/', views.PinDelete.as_view(), name='pins_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Pin
 
 from django.http import HttpResponse
 
+class PinCreate(CreateView):
+  model = Pin
+  fields = '__all__'
+
+class PinUpdate(UpdateView):
+  model = Pin
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['description', 'price']
+
+class PinDelete(DeleteView):
+  model = Pin
+  success_url = '/pins/'
+
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1> Hello </h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
